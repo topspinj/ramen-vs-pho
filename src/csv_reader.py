3027d233e0a76e5a2916fe59b647fde32c5ec7d2
Jill Cates <cates.jill@gmail.com>
2017-11-30 16:21:06 -0800

    Clean up data

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-ramen = pd.read_csv("https://s3.ca-central-1.amazonaws.com/ramenvspho/ramen_reviews.csv")
-pho = pd.read_csv("https://s3.ca-central-1.amazonaws.com/ramenvspho/pho_reviews.csv")
+data = pd.read_csv("https://s3.ca-central-1.amazonaws.com/ramenvspho/ramen_pho_count.csv")
 
-ramen.to_csv("../data/ramen_reviews.csv")
-pho.to_csv("../data/pho_reviews.csv")
+data.to_csv("../data/ramen_pho_count.csv")
