01634d5e1ad148f1b05f2b13f5b517645ddf0e1d
Jill Cates <cates.jill@gmail.com>
2017-12-03 21:35:37 -0800

    Update csv reader to pull aws data

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -3,3 +3,7 @@ import pandas as pd
 data = pd.read_csv("https://s3.ca-central-1.amazonaws.com/ramenvspho/ramen_pho_count.csv")
 
 data.to_csv("../data/ramen_pho_count.csv")
+
+major_city_data = pd.read_csv("https://s3.ca-central-1.amazonaws.com/ramenvspho/major_city_data_2015.csv")
+
+major_city_data.to_csv("../data/major_city_data_2015.csv")
